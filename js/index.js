ace0afef90b91d67dc442e0be16d89e6b3851a1c
Crzdev-avocado <85311593+Crzdev-avocado@users.noreply.github.com>
2021-07-06 12:40:58 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.sr= ScrollReveal();
+sr.reveal('.scroll-banner2', {
+	duration: 8000,
+	origin: 'left',
+	distance: '400px'
+
+	
+
+});
+sr.reveal('.scroll-banner3', {
+	duration: 5000,
+	origin: 'bottom',
+	distance: '400px'
+
+	
+
+});
+sr.reveal('.scroll-banner4', {
+	duration: 800,
+	origin: 'top',
+	distance: '400px'
+
+	
+
+});
